689ff4077855865909c7bedd211888feb4f6516f
Nicolau NP <mr1Normaliii@gmail.com>
2021-04-16 22:08:20 +0100

    update

--- database/seeds/DescontoSeeder.php
+++ database/seeds/DescontoSeeder.php
@@ -1,19 +1,30 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DescontoSeeder extends Seeder
 {
    static $descontos = [
        [
-        'desconto'=>"",
-        'preco',
-        'estado',
+        'id_modo'=>3,
+        'desconto'=>"Abertura de Conta",
+        'preco'=>100,
+        'estado'=>"on",
+       ], [
+        'id_modo'=>4,
+        'desconto'=>"Manutenção de Conta",
+        'preco'=>250,
+        'estado'=>"on",
        ]
    ];
    
     public function run()
     {
-        
+        foreach(Self::$descontos as $desconto){
+            DB::table('descontos')->insert(
+                $desconto
+            );
+        }
     }
 }
